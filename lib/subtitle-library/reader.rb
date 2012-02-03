78916e878784339d52f4145c562faa79a04466ce
Tony Rizov <bajanak@dir.bg>
2012-02-03 08:54:42 +0200

    Adding a method for reading/syntax checking of the sub rip format

--- lib/subtitle-library/reader.rb
+++ lib/subtitle-library/reader.rb
@@ -42,6 +42,109 @@ class SubsReader
     end
   end
 
+  def read_sub_rip(check_syntax)
+    actual_lines = 0
+    error_log = ""
+    is_eof = false
+    last_end_time = Time.new 1, 1, 1
+    File.open(@subs_path, 'r') do |subs|
+      while true
+        line = subs.gets
+        actual_lines += 1
+        break unless line
+        strip_line = line.strip
+        while strip_line == '' or /\d+$/ =~ strip_line
+          actual_lines += 1
+          line = subs.gets
+          unless line
+            is_eof = true
+            break
+          end
+          strip_line = line.strip
+        end
+        break if is_eof
+        if SUB_RIP_LINE =~ strip_line
+          match = SUB_RIP_TIMING.match(strip_line)
+          time_args = [1,1,1] + match.to_s.split(/,|:/).collect(&:to_i)
+          time_args[6] *= 1000
+          start_time = Time.mktime *time_args
+          match = SUB_RIP_TIMING.match match.post_match
+          time_args = [1,1,1] + match.to_s.split(/,|:/).collect(&:to_i)
+          time_args[6] *= 1000
+          end_time = Time.mktime *time_args
+          if start_time.day + end_time.day != 2 or start_time >= end_time or start_time < last_end_time
+            if check_syntax
+              error_log += "Invalid timing at #{actual_lines}.\n"
+            else
+              puts "Invalid timing at #{actual_lines}.\n"
+            end
+            break
+          end
+          last_end_time = end_time
+          line = subs.gets
+          unless line
+            @cues << Cue.new(start_time, end_time, '') unless check_syntax
+            break
+          end
+          text = line
+          actual_lines += 1
+          strip_line = line.strip
+          while strip_line != ''
+            actual_lines += 1
+            line = subs.gets
+            unless line
+              is_eof = True
+              break
+            end
+            text += line
+            strip_line = line.strip
+          end
+          if is_eof
+            @cues << Cue.new(start_time, end_time, text.rstrip) unless check_syntax
+            break
+          end
+          line = subs.gets
+          unless line
+            @cues << Cue.new(start_time, end_time, text.rstrip) unless check_syntax
+            break
+          end
+          actual_lines += 1
+          strip_line = line.strip
+          while not /\A\d+$/ =~ strip_line
+            text += line
+            actual_lines += 1
+            line = subs.gets
+            unless line
+              @cues << Cue.new(start_time, end_time, text.rstrip) unless check_syntax
+              is_eof = True
+              break
+            end
+            strip_line = line.strip
+          end
+          break if is_eof
+        elsif check_syntax
+          error_log += "Syntax error at line #{actual_lines}.\n"
+        else
+          line = subs.gets
+          break unless line
+          strip_line = line.strip
+          while not /\d+$/ =~ strip_line
+            line = subs.gets
+            unless line
+              is_eof = true
+              break
+            end
+            strip_line = line.strip
+          end
+          break if is_eof
+        end
+      end
+    end
+    if check_syntax
+      error_log == '' ? 'No errors were found.' : error_log
+    end
+  end
+
   def read_micro_dvd(check_syntax)
     actual_lines = 0
     error_log = ""
@@ -92,3 +195,5 @@ class SubsReader
     end
   end
 end
+
+puts 'a'
