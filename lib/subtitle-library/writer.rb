c1658cbdaa196a3873c9c905b9b0f5f1eb5d402c
Tony Rizov <bajanak@dir.bg>
2012-02-04 11:47:12 +0200

    Adding a method for saving subtitles from microdvd to subrip or subviewer format in the SubsWriter class

--- lib/subtitle-library/writer.rb
+++ lib/subtitle-library/writer.rb
@@ -30,6 +30,32 @@ class SubsWriter
     end
   end
 
+  def save_frames_to_timing(new_path, new_type)
+    line_count = 1
+    bef_mil, bet_start_end = new_type == 'sr' ? [',', ' --> '] : ['.', ',']
+    File.open(@subs_path, 'w') do |subs|
+      subs.write("[STYLE]no\n") if sub_type == 'sv'
+      @cues.each do |cue|
+        start = Time.mktime(1,1,1) + cue.start / fps
+        ending = Time.mktime(1,1,1) + cue.ending / fps
+        timing_line = start.to_s.split(' ')[1] + bef_mil + ("%03d" % (start.usec / 1000)) + bet_start_end
+        timing_line += ending.to_s.split(' ')[1] + bef_mil + ("%03d" % (ending.usec / 1000))
+        if start.year + start.month + start.day + ending.year + ending.month + ending.day != 6
+          puts 'Invalid timing'
+          break
+        end
+        if new_type == 'sr'
+          subs.write(line_count.to_s + "\n" + timing_line + "\n")
+          subs.write(cue.text + "\n\n")
+          line_count += 1
+        else
+          subs.write(timing_line + "\n")
+          subs.write(cue.text.gsub("\n", '[br]') + "\n\n")
+        end
+      end
+    end
+  end
+
   def save_timing_to_frames(new_path)
     File.open(@subs_path, 'w') do |subs|
       subs.write('{1}{1}' + @fps.to_s + "\n")
