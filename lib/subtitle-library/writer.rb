b02e232238a1ec3a76d48f41afdb01064f3ae880
Tony Rizov <bajanak@dir.bg>
2012-02-04 10:41:57 +0200

    Adding a method for saving subtitles in the SubsWriter class

--- lib/subtitle-library/writer.rb
+++ lib/subtitle-library/writer.rb
@@ -4,4 +4,20 @@ class SubsWriter
     @fps = subs_reader.fps
     @subs_type = subs_reader.type
   end
+
+  def save_as(new_path, new_type)
+    if @subs_type == 'md'
+      if new_type == 'md'
+        save_frames_to_frames new_path
+      else
+        save_frames_to_timing new_path, new_type
+      end
+    else
+      if new_type == 'md'
+        save_timing_to_frames new_path
+      else
+        save_timing_to_timing new_path, new_type
+      end
+    end
+  end     
 end
