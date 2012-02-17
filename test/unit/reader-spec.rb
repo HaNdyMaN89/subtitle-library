70324ca581f7c73c726df153c1c24691f47d8b98
Tony Rizov <bajanak@dir.bg>
2012-02-17 16:01:02 +0200

    Adding some tests for the subviewer reader class

--- test/unit/reader-spec.rb
+++ test/unit/reader-spec.rb
@@ -379,7 +379,7 @@ describe SubRipReader do
       end
     end
 
-    it 'loads all cues when syntax is incorrect' do
+    it 'loads the valid cues when syntax is incorrect' do
       FakeFS do
         File.open(path, 'w') do |subs|
           subs.write(<<-eos
@@ -418,6 +418,333 @@ describe SubRipReader do
                       00:02:07,840 --> 00:02:03,831
                       That'll teach you to excite
                       yourself like this.
+                      eos
+                      )
+        end
+        cues = []
+        cues << Cue.new(Time.mktime(1, 1, 1, 0, 2, 6, 320000), Time.mktime(1, 1, 1, 0, 2, 7, 639000), "Yes, but I have to go.")
+
+        new_reader(path).read_subs(false)[0].should eq cues
+      end
+    end
+
+  end
+end
+
+describe SubviewerReader do
+  include FakeFS
+
+  def setup
+    FakeFS.activate!
+    FileSystem.clear
+  end
+
+  def teardown
+    FakeFS.deactivate!
+  end
+
+  def new_reader(path)
+    SubviewerReader.new path
+  end
+
+  describe 'syntax checking' do
+    path = 'subs.sub'
+
+    it 'validates correct syntax' do
+      FakeFS do
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:02:05.593
+                      It was funny, huh?
+
+                      00:02:06.320,00:02:07.639
+                      Yes, but I have to go.
+
+                      00:02:07.840,00:02:09.831
+                      That'll teach you to excite yourself like this.
+
+                      00:02:10.040,00:02:12.508
+                      Stop somewhere if you can.
+
+                      00:02:43.560,00:02:46.028
+                      Honey, you're not at school.[br]Don't bother poor Elly.
+                    eos
+                    )
+        end
+        new_reader(path).read_subs(true).should eq "No errors were found."
+
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:2:5.593
+                      It was funny, huh?
+
+                      00:02:06.20,00:02:07.639
+                      Yes, but I have to go.
+
+                      00:2:07.840,00:02:09.831
+                      That'll teach you to excite yourself like this.
+
+                      00:02:10.00,00:02:12.5
+                      Stop somewhere if you can.
+                    eos
+                    )
+        end
+        new_reader(path).read_subs(true).should eq "No errors were found."
+
+      end
+      
+    end
+
+    it 'validates invalid timing' do
+      FakeFS do
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:02:04.003
+                      It was funny, huh?
+
+                      00:02:06.320,00:02:07.639
+                      Yes, but I have to go.
+
+                      00:02:07.840,00:02:09.831
+                      That'll teach you to excite yourself like this.
+
+                      00:02:10.040,00:02:12.508
+                      Stop somewhere if you can.
+
+                      00:02:43.560,00:02:46.028
+                      Honey, you're not at school.[br]Don't bother poor Elly.
+                    eos
+                    )
+        end
+
+        new_reader(path).read_subs(true).should eq "Invalid timing at line 1."
+
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:02:05.593
+                      It was funny, huh?
+
+                      00:02:06.320,00:02:06.320
+                      Yes, but I have to go.
+
+                      00:02:07.840,00:02:09.831
+                      That'll teach you to excite yourself like this.
+
+                      00:02:10.040,00:02:12.508
+                      Stop somewhere if you can.
+
+                      00:02:43.560,00:02:46.028
+                      Honey, you're not at school.[br]Don't bother poor Elly.
+                    eos
+                    )
+        end
+
+        new_reader(path).read_subs(true).should eq "Invalid timing at line 4."
+
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:02:03.593
+                      It was funny, huh?
+
+                      00:02:06.320,00:02:07.639
+                      Yes, but I have to go.
+
+                      00:02:07.840,00:02:07.831
+                      That'll teach you to excite yourself like this.
+
+                      00:02:10.040,00:02:12.508
+                      Stop somewhere if you can.
+
+                      00:02:43.560,00:02:46.028
+                      Honey, you're not at school.[br]Don't bother poor Elly.
+                    eos
+                    )
+        end
+
+        new_reader(path).read_subs(true).should eq "Invalid timing at line 1.\nInvalid timing at line 7."
+      end
+      
+    end
+
+    it 'validates invalid start-end time format' do
+      FakeFS do
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:02:05.593
+                      It was funny, huh?
+
+                      00:02:06320,00:02:07.639
+                      Yes, but I have to go.
+
+                      00:02:07.840,00:02:09.831
+                      That'll teach you to excite yourself like this.
+
+                      00:02:10.040,00:02:12.508
+                      Stop somewhere if you can.
+
+                      00:02:43.560,00:02:46.028
+                      Honey, you're not at school.[br]Don't bother poor Elly.
+                    eos
+                    )
+        end
+
+        new_reader(path).read_subs(true).should eq "Syntax error at line 4."
+
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:02:05593
+                      It was funny, huh?
+
+                      00:02:06.320,00:02:07.639
+                      Yes, but I have to go.
+
+                      00:02:07.84000:02:09.831
+                      That'll teach you to excite yourself like this.
+
+                      00:02:10.040,00:02:12.508
+                      Stop somewhere if you can.
+
+                      00:02:43.560,00:02:46.028
+                      Honey, you're not at school.[br]Don't bother poor Elly.
+                    eos
+                    )
+        end
+
+        new_reader(path).read_subs(true).should eq "Syntax error at line 1.\nSyntax error at line 7."
+
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:02:05.593
+                      It was funny, huh?
+
+                      00:02:06.320,00:02:07.639
+                      Yes, but I have to go.
+
+                      00:02:07.840,00:02:09.831
+                      That'll teach you to excite yourself like this.
+
+                      00:02:10.040,00:02:12.508
+                      Stop somewhere if you can.
+
+                      00:02:43,560,00:02:46.028
+                      Honey, you're not at school.[br]Don't bother poor Elly.
+                    eos
+                    )
+        end
+
+        new_reader(path).read_subs(true).should eq "Syntax error at line 13."
+      end
+      
+    end
+
+    it 'validates invalid text after a line of valid text' do
+      FakeFS do
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:02:05.593
+                      It was funny, huh?
+                      s
+
+                      00:02:06.320,00:02:07.639
+                      Yes, but I have to go.
+
+                      00:02:07.840,00:02:09.831
+                      That'll teach you to excite yourself like this.
+
+                      00:02:10.040,00:02:12.508
+                      Stop somewhere if you can.
+
+                      00:02:43.560,00:02:46.028
+                      Honey, you're not at school.[br]Don't bother poor Elly.
+                    eos
+                    )
+        end
+        new_reader(path).read_subs(true).should eq "Syntax error at line 3."
+
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:02:05.593
+                      It was funny, huh?
+
+                      00:02:06.320,00:02:07.639
+                      Yes, but I have to go.
+
+                      00:02:07.840,00:02:09.831
+                      That'll teach you to excite yourself like this.
+                      s
+
+                      00:02:10.040,00:02:12.508
+                      Stop somewhere if you can.
+
+                      00:02:43.560,00:02:46.028
+                      Honey, you're not at school.[br]Don't bother poor Elly.
+                    eos
+                    )
+        end
+        new_reader(path).read_subs(true).should eq "Syntax error at line 9."
+
+      end
+    end
+  end
+
+  describe 'cue loading' do
+    path = 'subs.sub'
+
+    it 'loads all cues when syntax is correct' do
+      FakeFS do
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:02:05.593
+                      It was funny, huh?
+
+                      00:02:06.320,00:02:07.639
+                      Yes, but I have to go.
+
+                      00:02:07.840,00:02:09.831
+                      That'll teach you to excite[br]yourself like this.
+                      eos
+                      )
+        end
+        cues = []
+        cues << Cue.new(Time.mktime(1, 1, 1, 0, 2, 4, 240000), Time.mktime(1, 1, 1, 0, 2, 5, 593000), "It was funny, huh?")
+        cues << Cue.new(Time.mktime(1, 1, 1, 0, 2, 6, 320000), Time.mktime(1, 1, 1, 0, 2, 7, 639000), "Yes, but I have to go.")
+        cues << Cue.new(Time.mktime(1, 1, 1, 0, 2, 7, 840000), Time.mktime(1, 1, 1, 0, 2, 9, 831000), "That'll teach you to excite\nyourself like this.")
+        
+        new_reader(path).read_subs(false)[0].should eq cues
+      end
+    end
+
+    it 'loads all cues when syntax is incorrect' do
+      FakeFS do
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04.240,00:02:05.593
+                      It was funny, huh?
+
+                      00:02:06.320.00:02:07.639
+                      Yes, but I have to go.
+
+                      00:02:07.840,00:02:09.831
+                      That'll teach you to excite[br]yourself like this.
+                      eos
+                      )
+        end
+        cues = []
+        cues << Cue.new(Time.mktime(1, 1, 1, 0, 2, 4, 240000), Time.mktime(1, 1, 1, 0, 2, 5, 593000), "It was funny, huh?")
+        cues << Cue.new(Time.mktime(1, 1, 1, 0, 2, 7, 840000), Time.mktime(1, 1, 1, 0, 2, 9, 831000), "That'll teach you to excite\nyourself like this.")
+        
+        new_reader(path).read_subs(false)[0].should eq cues
+
+        File.open(path, 'w') do |subs|
+          subs.write(<<-eos
+                      00:02:04240,00:02:05.593
+                      It was funny, huh?
+
+                      00:02:06,320,00:02:07,639
+                      Yes, but I have to go.
+
+                      00:02:07.840,00:02:.831
+                      That'll teach you to excite[br]yourself like this.
                       eos
                       )
         end
