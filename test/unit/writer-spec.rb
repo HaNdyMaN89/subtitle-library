9b714525fc184d116b754c3278dfafa2dc2cf123
Tony Rizov <bajanak@dir.bg>
2012-02-17 19:34:50 +0200

    Adding tests for saving from subrip and subviewer format

--- test/unit/writer-spec.rb
+++ test/unit/writer-spec.rb
@@ -19,53 +19,297 @@ describe SubsWriter do
     SubsWriter.new (SubsReader.new path)
   end
 
-  describe 'saving from microdvd to microdvd format' do
-    path = 'subs.sub'
+  describe 'saving from subrip to subrip format' do
+    path = 'subs.srt'
 
     it 'saves the file as is when syntax is correct' do
       FakeFS do
-        lines = "{5277}{5309}You want some water with that?\n"
-        lines += "{5311}{5345}No, no. No, I don't.\n"
-        lines += "{5362}{5396}Looks like you had a night.\n"
+        lines = "1\n"
+        lines += "00:03:40,095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "2\n"
+        lines += "00:03:41,513 --> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "3\n"
+        lines += "00:03:43,640 --> 00:03:45,058\n"
+        lines += "Looks like you had a night.\n\n"
 
         File.open(path, 'w') do |subs|
           subs.write(lines)
         end
-
-        new_writer(path).save_as(path, 'md')
-
-        lines = "{1}{1}23.976\n" + lines
+        
+        new_writer(path).save_as(path, 'sr')
 
         File.open(path, 'r') do |subs|
           subs.read.should eq lines
         end
+
       end
     end
 
-    it 'saves only the valid cues when syntax is incorrect' do
+    it 'saves the valid cues when syntax is incorrect' do
       FakeFS do
-        lines = "{5277}{5309}You want some water with that?\n"
-        lines += "{5311}5345}No, no. No, I don't.\n"
-        lines += "{5362}{5396}Looks like you had a night.\n"
+        lines = "1\n"
+        lines += "00:03:40095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "2\n"
+        lines += "00:03:41,513 --> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "3\n"
+        lines += "00:03:43,640 --> 00:03:45,058\n"
+        lines += "Looks like you had a night.\n\n"
 
         File.open(path, 'w') do |subs|
           subs.write(lines)
         end
+        
+        new_writer(path).save_as(path, 'sr')
 
+        lines = "1\n"
+        lines += "00:03:41,513 --> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "2\n"
+        lines += "00:03:43,640 --> 00:03:45,058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+        lines = "1\n"
+        lines += "00:03:40,095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "2\n"
+        lines += "00:03:41,513 -> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "3\n"
+        lines += "00:03:43,640 --> 0003:45,058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'sr')
+
+        lines = "1\n"
+        lines += "00:03:40,095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+      end
+    end
+  end
+
+  describe 'saving from subrip to microdvd format' do
+    path = 'subs.sub'
+
+    it 'saves all the cues when syntax is correct' do
+      FakeFS do
+        lines = "1\n"
+        lines += "00:03:40,095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "2\n"
+        lines += "00:03:41,513 --> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "3\n"
+        lines += "00:03:43,640 --> 00:03:45,058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
         new_writer(path).save_as(path, 'md')
 
-        lines = "{1}{1}23.976\n" + "{5277}{5309}You want some water with that?\n"
+        lines = "{1}{1}23.976\n"
+        lines += "{5277}{5309}You want some water with that?\n"
+        lines += "{5311}{5345}No, no.|No, I don't.\n"
         lines += "{5362}{5396}Looks like you had a night.\n"
 
         File.open(path, 'r') do |subs|
           subs.read.should eq lines
         end
+
+        lines = "1\n"
+        lines += "00:03:40,095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "2\n"
+        lines += "00:03:41,513 --> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "3\n"
+        lines += "00:03:43,640 --> 00:03:45,058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'md', 25)
+
+        lines = "{1}{1}25.0\n"
+        lines += "{5503}{5536}You want some water with that?\n"
+        lines += "{5538}{5574}No, no.|No, I don't.\n"
+        lines += "{5591}{5627}Looks like you had a night.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+      end
+    end
+
+    it 'saves the valid cues when syntax is incorrect' do
+      FakeFS do
+        lines = "1\n"
+        lines += "00:03:40,095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "2\n"
+        lines += "00:03:41,513 -> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "3\n"
+        lines += "00:03:43,640 --> 00:03:45,058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'md')
+
+        lines = "{1}{1}23.976\n"
+        lines += "{5277}{5309}You want some water with that?\n"
+        lines += "{5362}{5396}Looks like you had a night.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+        lines = "1\n"
+        lines += "00:03:40095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "2\n"
+        lines += "00:03:41,513 --> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "3\n"
+        lines += "00:03:43,640 --> 00:035,058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'md', 25)
+
+        lines = "{1}{1}25.0\n"
+        lines += "{5538}{5574}No, no.|No, I don't.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+      end
+
+    end
+  end
+
+  describe 'saving from subrip to subviewer format' do
+    path = 'subs.sub'
+
+    it 'saves all the cues when syntax is correct' do
+      FakeFS do
+        lines = "1\n"
+        lines += "00:03:40,095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "2\n"
+        lines += "00:03:41,513 --> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "3\n"
+        lines += "00:03:43,640 --> 00:03:45,058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'sv')
+
+        lines = "[STYLE]no\n"
+        lines += "00:03:40.095,00:03:41.429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "00:03:41.513,00:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.640,00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+      end
+    end
+
+    it 'saves the valid cues when syntax is incorrect' do
+      FakeFS do
+        lines = "1\n"
+        lines += "00:03:40095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "2\n"
+        lines += "00:03:41,513 --> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "3\n"
+        lines += "00:03:43,640 --> 00:03:45,058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'sv')
+
+        lines = "[STYLE]no\n"
+        lines += "00:03:41.513,00:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.640,00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+        lines = "1\n"
+        lines += "00:03:40,095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "2\n"
+        lines += "00:03:41,513 -> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "3\n"
+        lines += "00:03:43,640 --> 0003:45,058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'sv')
+
+        lines = "[STYLE]no\n"
+        lines += "00:03:40.095,00:03:41.429\n"
+        lines += "You want some water with that?\n\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
       end
     end
   end
 
   describe 'saving from microdvd to subrip format' do
-    path = 'subs.sub'
+    path = 'subs.srt'
 
     it 'saves all the cues when syntax is correct' do
       FakeFS do
@@ -168,6 +412,51 @@ describe SubsWriter do
     end
   end
 
+  describe 'saving from microdvd to microdvd format' do
+    path = 'subs.sub'
+
+    it 'saves the file as is when syntax is correct' do
+      FakeFS do
+        lines = "{5277}{5309}You want some water with that?\n"
+        lines += "{5311}{5345}No, no. No, I don't.\n"
+        lines += "{5362}{5396}Looks like you had a night.\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+
+        new_writer(path).save_as(path, 'md')
+
+        lines = "{1}{1}23.976\n" + lines
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+      end
+    end
+
+    it 'saves only the valid cues when syntax is incorrect' do
+      FakeFS do
+        lines = "{5277}{5309}You want some water with that?\n"
+        lines += "{5311}5345}No, no. No, I don't.\n"
+        lines += "{5362}{5396}Looks like you had a night.\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+
+        new_writer(path).save_as(path, 'md')
+
+        lines = "{1}{1}23.976\n" + "{5277}{5309}You want some water with that?\n"
+        lines += "{5362}{5396}Looks like you had a night.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+      end
+    end
+  end
+
   describe 'saving from microdvd to subviewer format' do
     path = 'subs.sub'
 
@@ -265,4 +554,273 @@ describe SubsWriter do
       end
     end
   end
+
+  describe 'saving from subviewer to subrip format' do
+    path = 'subs.srt'
+
+    it 'saves all the cues when syntax is correct' do
+      FakeFS do
+        lines = "00:03:40.095,00:03:41.429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "00:03:41.513,00:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.640,00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+
+        lines = "1\n"
+        lines += "00:03:40,095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "2\n"
+        lines += "00:03:41,513 --> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "3\n"
+        lines += "00:03:43,640 --> 00:03:45,058\n"
+        lines += "Looks like you had a night.\n\n"
+        
+        new_writer(path).save_as(path, 'sr')
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+      end
+    end
+
+    it 'saves the valid cues when syntax is incorrect' do
+      FakeFS do
+
+        lines = "00:03:40,095,00:03:41.429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "00:03:41.513,00:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.640,00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'sr')
+
+        lines = "1\n"
+        lines += "00:03:41,513 --> 00:03:42,931\n"
+        lines += "No, no.\nNo, I don't.\n\n"
+        lines += "2\n"
+        lines += "00:03:43,640 --> 00:03:45,058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+        lines = "00:03:40.095,00:03:41.429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "00:03:41.,00:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.64000:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'sr')
+
+        lines = "1\n"
+        lines += "00:03:40,095 --> 00:03:41,429\n"
+        lines += "You want some water with that?\n\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+      end
+    end
+  end
+
+  describe 'saving from subviewer to microdvd format' do
+    path = 'subs.sub'
+
+    it 'saves all the cues when syntax is correct' do
+      FakeFS do
+
+        lines = "00:03:40.095,00:03:41.429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "00:03:41.513,00:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.640,00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'md')
+
+        lines = "{1}{1}23.976\n"
+        lines += "{5277}{5309}You want some water with that?\n"
+        lines += "{5311}{5345}No, no.|No, I don't.\n"
+        lines += "{5362}{5396}Looks like you had a night.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+        lines = "00:03:40.095,00:03:41.429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "00:03:41.513,00:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.640,00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'md', 25)
+
+        lines = "{1}{1}25.0\n"
+        lines += "{5503}{5536}You want some water with that?\n"
+        lines += "{5538}{5574}No, no.|No, I don't.\n"
+        lines += "{5591}{5627}Looks like you had a night.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+      end
+    end
+
+    it 'saves the valid cues when syntax is incorrect' do
+      FakeFS do
+
+        lines = "00:03:40.095,00:03:41.429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "00:03:41.51300:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.640,00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'md')
+
+        lines = "{1}{1}23.976\n"
+        lines += "{5277}{5309}You want some water with that?\n"
+        lines += "{5362}{5396}Looks like you had a night.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+        lines = "00:03:40.095,0003:41.429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "00:03:41.513,00:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43;640,00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'md', 25)
+
+        lines = "{1}{1}25.0\n"
+        lines += "{5538}{5574}No, no.|No, I don't.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+      end
+
+    end
+  end
+
+  describe 'saving from subviewer to subviewer format' do
+    path = 'subs.sub'
+
+    it 'saves the file as is when syntax is correct' do
+      FakeFS do
+
+        lines = "00:03:40.095,00:03:41.429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "00:03:41.513,00:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.640,00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'sv')
+
+        lines = "[STYLE]no\n" + lines
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+      end
+    end
+
+    it 'saves the valid cues when syntax is incorrect' do
+      FakeFS do
+
+        lines = "00:03:40.095,00:03:34.429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "00:03:41.513,00:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.640,00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'sv')
+
+        lines = "[STYLE]no\n"
+        lines += "00:03:41.513,00:03:42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.640,00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+        lines = "00:03:40.095,00:03:41.429\n"
+        lines += "You want some water with that?\n\n"
+        lines += "00:03:41.513,00:03;42.931\n"
+        lines += "No, no.[br]No, I don't.\n\n"
+        lines += "00:03:43.640.00:03:45.058\n"
+        lines += "Looks like you had a night.\n\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+        
+        new_writer(path).save_as(path, 'sv')
+
+        lines = "[STYLE]no\n"
+        lines += "00:03:40.095,00:03:41.429\n"
+        lines += "You want some water with that?\n\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+      end
+    end
+  end
+
 end
