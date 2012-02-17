f0cd53f1bac7774cb0c4bc81b13a176b6d020a21
Tony Rizov <bajanak@dir.bg>
2012-02-17 21:31:35 +0200

    Adding tests for shifting subtitles of the microdvd format

--- test/unit/changer-spec.rb
+++ test/unit/changer-spec.rb
@@ -153,6 +153,104 @@ describe SubsChanger do
 
   end
 
+  describe 'shifting microdvd format' do
+    path = 'subs.sub'
+
+    it 'shifts the subtitles by frames' do
+      FakeFS do
+        lines = "{5277}{5309}You want some water with that?\n"
+        lines += "{5311}{5345}No, no.|No, I don't.\n"
+        lines += "{5362}{5396}Looks like you had a night.\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+
+        new_changer(path).shift('fs', 80)
+
+        lines = "{1}{1}23.976\n"
+        lines += "{5357}{5389}You want some water with that?\n"
+        lines += "{5391}{5425}No, no.|No, I don't.\n"
+        lines += "{5442}{5476}Looks like you had a night.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+        new_changer(path).shift('fs', -100)
+
+        lines = "{1}{1}23.976\n"
+        lines += "{5257}{5289}You want some water with that?\n"
+        lines += "{5291}{5325}No, no.|No, I don't.\n"
+        lines += "{5342}{5376}Looks like you had a night.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+      end
+    end
+
+    it 'shifts the subtitles by seconds' do
+      FakeFS do
+        lines = "{5277}{5309}You want some water with that?\n"
+        lines += "{5311}{5345}No, no.|No, I don't.\n"
+        lines += "{5362}{5396}Looks like you had a night.\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+
+        new_changer(path).shift('ss', 10)
+
+        lines = "{1}{1}23.976\n"
+        lines += "{5517}{5549}You want some water with that?\n"
+        lines += "{5551}{5585}No, no.|No, I don't.\n"
+        lines += "{5602}{5636}Looks like you had a night.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+        new_changer(path).shift('ss', -21.2, 25)
+
+        lines = "{1}{1}23.976\n"
+        lines += "{4987}{5019}You want some water with that?\n"
+        lines += "{5021}{5055}No, no.|No, I don't.\n"
+        lines += "{5072}{5106}Looks like you had a night.\n"
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+      end
+    end
+
+    it "doesn't shift when an invalid timing is calculated" do
+      FakeFS do
+        lines = "{5277}{5309}You want some water with that?\n"
+        lines += "{5311}{5345}No, no.|No, I don't.\n"
+        lines += "{5362}{5396}Looks like you had a night.\n"
+
+        File.open(path, 'w') do |subs|
+          subs.write(lines)
+        end
+
+        new_changer(path).shift('fs', -5300)
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+
+        new_changer(path).shift('ss', -212, 25)
+
+        File.open(path, 'r') do |subs|
+          subs.read.should eq lines
+        end
+      end
+    end
+
+  end
+
+
   describe 'shifting subviewer format' do
     path = 'subs.sub'
 
